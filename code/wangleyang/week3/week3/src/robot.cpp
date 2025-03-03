ea19eabf88264dc6960db4f2b4afea186cb25758
wly612 <3422383556@qq.com>
2025-03-03 23:34:24 +0800

    Revert "part2"
    This reverts commit da0df485daeef88f878efcf01c422360d5ed9aa7.

--- code/wangleyang/week3/week3/src/robot.cpp
+++ code/wangleyang/week3/week3/src/robot.cpp
@@ -2,30 +2,27 @@
 #include "robot.hpp"
 robot::robot(std::string name):NAME(name){}
 robot::~robot(){
-    for(auto it=tasks.begin();it!=tasks.end();it++){
-        const task& t=*it;
-    if(t.getstatus()=="待执行"){
-        std::cout<<"未执行任务ID: "<<t.getID()<<", 描述："<<t.getdescription()<<std::endl;
+    for(int i=0;i<tasks.size();i++){
+    if(tasks[i].getstatus()=="待执行"){
+        std::cout<<"未执行任务ID: "<<tasks[i].getID()<<", 描述："<<tasks[i].getdescription()<<std::endl;
     }
     }
 }
-void robot::addtask(int id, const std::string& description,int x){
-    task newtask(id,description,x);
-    tasks.insert(newtask);
+void robot::addtask(int id, const std::string& description){
+    tasks.emplace_back(id,description);
 }
 void robot::executetasks(){
-    auto it=tasks.end();
-    it--;
-    it->execute();
+    for(int i=0;i<tasks.size();i++){
+    tasks[i].execute();
     }
+}
+
 void robot::displaytasks(){
-    for(auto it=tasks.begin();it!=tasks.end();it++){
-        const task& t=*it;
-        std::cout<<"任务ID:"<<t.getID()<<", 描述："<<t.getdescription()<<", 状态："<<t.getstatus()<<std::endl;
+    for(int i=0;i<tasks.size();i++){
+        std::cout<<"任务ID:"<<tasks[i].getID()<<", 描述："<<tasks[i].getdescription()<<", 状态："<<tasks[i].getstatus()<<std::endl;
     }
 }
 
 void robot::remove(){
-    auto it=tasks.begin();
-    tasks.erase(it);
+    tasks.pop_back();
 }
