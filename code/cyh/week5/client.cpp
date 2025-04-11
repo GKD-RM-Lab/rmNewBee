203c68d63d2525f73e43dd31880cfd258eb1a515
nyiuu <3055316048@qq.com>
2025-04-11 14:36:12 +0800

    week5

--- code/cyh/week5/client.cpp
+++ code/cyh/week5/client.cpp
@@ -1,163 +1,124 @@
-/*客户端*/
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-#include<unistd.h>
-#include<netdb.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include"order.h"
-#include<vector>
+//客户端
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <thread>
+#include <mutex>
+#include <cstdlib>
+#include <ctime>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <cstring>
+#include <sstream>
 
 using namespace std;
 
-class ctcpclient{
-    public:
-        int m_clientfd; //客户端socket，-1表示无效, 0表示有效
-        string m_ip; //服务端IP/域名
-        unsigned short m_port; //通讯端口
-
-        ctcpclient() : m_clientfd(-1){}
-
-//向服务端发起连接请求，成功返回true，失败返回false
-        bool connect(const string& in_ip, const unsigned short in_port){
-            if(m_clientfd != -1){
-               return false;
-            }
-
-            m_ip = in_ip;
-            m_port = in_port;
-            /*创建客户端的socket*/    
-            if((m_clientfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-                return false;
-            }
-            /*向服务器发起连接请求*/
-        
-            struct sockaddr_in servaddr;  //存放服务端IP和端口的结构体
-            memset(&servaddr, 0, sizeof(servaddr)); // 将 servaddr的所有字节初始化为零
-            servaddr.sin_family = AF_INET; //IPV4
-            servaddr.sin_port = htons(m_port); //指定通信端口， h to ns，h->host, n->network, s->short 
-            struct hostent* h;//存放IP的结构体 
-            if((h = gethostbyname(m_ip.c_str())) == nullptr){  //字符串转换为结构体
-                ::close(m_clientfd);
-                m_clientfd = -1;
-                return false;
-            }       
-            memcpy(&servaddr.sin_addr, h->h_addr, h->h_length); //指定IP
-           
-            if(::connect(m_clientfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1){ //发起连接请求
-                ::close(m_clientfd);
-                m_clientfd = -1;
-                return false;
-            }
-            return true;
+vector<int> menuItems;
+mutex coutMutex;
+// 读取菜单
+void loadMenu(const string& filename) {
+    ifstream file(filename);
+    for (string line; getline(file, line); ) {
+        istringstream iss(line);
+        int num;
+        if (iss >> num) {  // 自动读取第一个数字（制表符前的内容）
+            menuItems.push_back(num);
         }
-//向服务器发送报文，成功返回true，失败返回false
-        bool send(const string &buffer, int size){ //buffer不要用const char*,这样兼容性更好
-            if(m_clientfd == -1){
-                return false;
-            }
-            if((write(m_clientfd, buffer.data(), size)) <= 0){
-                return false;
-            }
-            return true;
-        }
-        bool recv(string &buffer, const size_t maxlen){
-            buffer.clear();
-            buffer.resize(maxlen);
+    }
+}
 
-            int readn = read(m_clientfd, &buffer[0], buffer.size()); //buffer.data()返回的是const的，这里我们可能直接修改buffer，因此不用那种
-            if(readn <= 0){ //-1失败，0连接断开
-                buffer.clear();
-                return false;
-            }
-            buffer.resize(readn);
-            return true;
-        }
-        bool close(){
-            if(m_clientfd == -1){
-                return false;
-            }
-            ::close(m_clientfd);
-            m_clientfd = -1;
-            return true;
-        }
-        ~ ctcpclient(){
-            close();
-        }
-};
+void placeOrder(int customerId) {
+    this_thread::sleep_for(chrono::seconds(1 + rand() % 5)); //睡眠1-5秒
 
-
-int main(int argc, char *argv[]){
-//确保只给了IP和端口两个参数
-    if(argc != 3){
-        cout << "Using:./client 服务端的IP 服务端的端口\nEample:./client 127.0.0.1 5005\n" << endl;
-        return -1;
-    } 
-    ctcpclient tcpclient;
-    if(tcpclient.connect(argv[1],atoi(argv[2])) == false){
-        perror("connect");
-        return -1;
+    // 生成订单
+    vector<int> order;
+    int items = 1 + rand() % 3;
+    for (int i = 0; i < items; ++i) {
+        order.push_back(menuItems[rand() % menuItems.size()]);
     }
 
-/*进行通讯*/
-    for(int ii = 1; ii < 2; ii++){
-        //生成请求报文的内容
-        // buffer = "这是第" + to_string(ii + 1) + "条消息, 编号" + to_string(ii + 1) + ". ";
-        
-        int item_count = 1;
-        int item_sum = 1;
-        int customerid = 1;
-        vector<int> item_quantity;
-        item_quantity.push_back(1);
-
-        MessageHeader msgheader;
-        msgheader.msgType = ORDER_REQUEST;
-        msgheader.msgLength = sizeof(MessageHeader) + sizeof(OrderHeader) + item_count * sizeof(OrderItem);
-
-        OrderHeader orderheader;
-        orderheader.customerId = customerid;
-        orderheader.itemCount = item_sum;
-
-        vector<OrderItem> items(item_sum);
-        items[item_count - 1].foodId = 1;
-        items[item_count - 1].quantity = item_quantity[item_count - 1];
-
-        string buffer;
-        int offset = 0;
-
-        buffer.resize(msgheader.msgLength );
-        memcpy(&buffer[offset], &msgheader, sizeof(MessageHeader));
-        offset += sizeof(MessageHeader);
-        memcpy(&buffer[offset], &orderheader, sizeof(OrderHeader));
-        offset += sizeof(OrderHeader);
-      
-        
-        for (int i = 0; i < orderheader.itemCount; i++) {
-            memcpy(&buffer[offset], &items[i], sizeof(OrderItem));
-            offset += sizeof(OrderItem);
-        }
-
-        for (char c : buffer) {
-            printf("%02x ", static_cast<unsigned char>(c));
-        }
-        printf("\n");
-        
-        //发送请求报文
-            if(tcpclient.send(buffer, msgheader.msgLength) == false){
-                perror("send");
-                break;
-        }
-        offset = 0;
-        cout << "发送：" << buffer << endl;
-        //接收服务端的报文,如果没有发送，recv()函数阻塞
-            if((tcpclient.recv(buffer, 1024)) <= 0){
-            perror("recv()");
-            break;
-        }
-        cout << "接收：" << buffer << endl;
+    // 创建socket
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        lock_guard<mutex> lock(coutMutex);
+        cerr << "顾客 " << customerId << ": 创建套接字失败" << endl;
+        return;
     }
-    close(tcpclient.m_clientfd);
+    //关闭端口  
+    int reuse = 1;
+    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+        shutdown(sock, SHUT_RDWR);
+        close(sock);
+        return;
+    }
+
+    // 设置连接超时（5秒）
+    timeval timeout{5, 0};
+    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
+    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
+
+    // 连接服务器
+    sockaddr_in servAddr{};
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &servAddr.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
+        lock_guard<mutex> lock(coutMutex);
+        cerr << "顾客 " << customerId << ": 连接服务器失败 - " << strerror(errno) << endl;
+        shutdown(sock, SHUT_RDWR);
+        close(sock);
+        return;
+    }
+
+    // 准备订单数据
+    stringstream orderStream;
+    for (size_t i = 0; i < order.size(); ++i) {
+        if (i > 0) orderStream << ",";
+        orderStream << order[i];
+    }
+    string orderStr = orderStream.str();
+
+    // 发送订单
+    if (send(sock, orderStr.c_str(), orderStr.size(), 0) < 0) {
+        lock_guard<mutex> lock(coutMutex);
+        cerr << "顾客 " << customerId << ": 发送订单失败" << endl;
+        shutdown(sock, SHUT_RDWR);
+        close(sock);
+        return;
+    }
+
+    // 接收响应
+    char buffer[1024] = {0};
+    int bytesReceived = recv(sock, buffer, sizeof(buffer), 0);
+    if (bytesReceived <= 0) {
+        lock_guard<mutex> lock(coutMutex);
+        cerr << "顾客 " << customerId << ": 未收到服务器响应" << endl;
+    } else {
+        lock_guard<mutex> lock(coutMutex);
+        cout << "顾客 " << customerId << ": 订单 " << orderStr 
+             << " " << (buffer[0] == '1' ? "成功" : "失败") << endl;
+    }
+    shutdown(sock, SHUT_RDWR);
+    close(sock);
+}
+
+
+int main() {
+    srand(time(nullptr));
+    loadMenu("foods.txt");
+
+    const int cunstomer_nums = 10;
+    vector<thread> customers;
+
+    for (int i = 0; i < cunstomer_nums; ++i) {
+        customers.emplace_back(placeOrder, i+1);
+    }
+
+    for (auto& t : customers) {
+        t.join();
+    }
+
+    return 0;
 }
--- code/cyh/week5/server.cpp
+++ code/cyh/week5/server.cpp
@@ -1,197 +1,235 @@
-/*服务端*/
-
-#include<iostream>
-#include<cstdio>
-#include<cstring>
-#include<cstdlib>
-#include<unistd.h>
-#include<netdb.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include"order.h"
+//服务器端
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <map>
+#include <mutex>
+#include <thread>
+#include <ctime>
+#include <algorithm>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
 using namespace std;
 
-class ctcpserver{
-    public:
-        int m_listenfd;
-        int m_clientfd;
-        string clientip;
-        unsigned short m_port;
+// 食材信息
+struct FoodItem {
+    int id;
+    string name;
+    vector<string> ingredients;
+};  
 
-        ctcpserver() : m_listenfd(-1), m_clientfd(-1){}
-//服务端初始化
-        bool init_server(const unsigned short in_port){
-            /*创建服务端socket*/
-            if((m_listenfd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
-                return false;
-            }
-            m_port = in_port;
-           /*把服务端用于通信的IP和端口绑定到socket上*/
-            struct sockaddr_in servaddr;  //存放服务端IP和端口的结构体
-            memset(&servaddr, 0, sizeof(servaddr)); // 将 servaddr的所有字节初始化为零
-            servaddr.sin_family = AF_INET; //IPV4
-            servaddr.sin_addr.s_addr = htonl(INADDR_ANY); //服务端任意的IP都可以通讯
-            servaddr.sin_port = htons(m_port); //指定通信端口
-           /*socket设置为可监听状态*/
-            if(bind(m_listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) == -1){
-                close(m_listenfd);
-                m_clientfd = -1;
-                return false;
-            }
-            if(listen(m_listenfd, 5) != 0){
-                close(m_listenfd);
-                m_listenfd = -1;
-                return false;
-            }
-            return true;
+// 库存信息
+map<int, FoodItem> foods;
+map<string, int> inventory;
+mutex inventory_mutex;
+mutex log_mutex;
+
+//读取食材信息的分割函数
+vector<string> split(const string &s, char delimiter) {
+    vector<string> tokens;
+    string token;
+    istringstream tokenStream(s); //将s转换为字符串流，以便按分隔符分割
+    while (getline(tokenStream, token, delimiter)) {
+        if (!token.empty()) {
+            tokens.push_back(token);
         }
-//受理客户端的连接请求
-        bool accept(){
-            struct sockaddr_in caddr;
-            socklen_t addrlen = sizeof(caddr);
-            m_clientfd = ::accept(m_listenfd, (struct sockaddr *)& caddr, &addrlen);
-            if(m_clientfd == -1){
-                close(m_listenfd);
-               return false;
-            }      
-            return true;
-        }
- //发送报文
-        bool send(const string& buffer){
-            if(m_clientfd == -1){
-                return false;
+    }
+    return tokens;
+}
+//读取食材信息
+void loadFoods(const string &filename) {
+    ifstream file(filename);
+    string line;
+    while (getline(file, line)) {
+        vector<string> parts = split(line, '\t');
+        if (parts.size() < 3) continue;
+
+        FoodItem item;
+        item.id = stoi(parts[0]);
+        item.name = parts[1];
+        // 分割食材部分
+        vector<string> ingredients = split(parts[2], ' ');
+        for (const string& ing : ingredients) {
+            if (!ing.empty()) {
+                item.ingredients.push_back(ing);
             }
-            if((write(m_clientfd, buffer.data(), buffer.size())) <= 0){
-               return false;
-            }
-            return true;
         }
-//接受报文
-        bool recv(string& buffer, const size_t maxlen){
-            buffer.clear();
-            buffer.resize(maxlen);
-            int readn = read(m_clientfd, &buffer[0], buffer.size()); //buffer.data()返回的是const的，这里我们可能直接修改buffer，因此不用那种
-            if(readn <= 0){ //-1失败，0连接断开
-                buffer.clear();
-                return false;
-            }
-            buffer.resize(readn);
-            return true;
+        foods[item.id] = item;
+    }
+    file.close();
+}
+
+//读取库存信息(不用加锁，因为只在开头调用一次)
+void loadInventory(const string &filename) {
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "无法打开: " << filename << endl;
+        exit(1);
+    }
+
+    string line;
+    while (getline(file, line)) {
+        // 跳过空行
+        if (line.empty()){
+            continue;
+        } 
+        
+        stringstream ss(line);
+        string name;
+        int quantity;
+        
+        if (ss >> name >> quantity) {
+            inventory[name] = quantity;
+            cout << "Loaded inventory: " << name << " = " << inventory[name] << endl;
+        } else {
+            cerr << "inventory.txt格式错误: " << line << endl;
         }
-//关闭socket
-        bool close_listen(){
-            if(m_listenfd == -1){
-                return false;
-            }
-            ::close(m_listenfd);
-            m_listenfd = -1;
-            return true;
+    }
+    file.close();
+}
+
+string getCurrentTime() {
+    time_t now = time(0); //获取当前时间
+    tm *ltm = localtime(&now);  //转换为本地时间
+    char buf[20];
+    strftime(buf, sizeof(buf), "%H-%M-%S", ltm);
+    return string(buf);
+}
+
+//记录订单信息
+void logOrder(const vector<int>& order, bool success, const map<string, int>& updates) {
+    lock_guard<mutex> lock(log_mutex);
+    ofstream logfile("kitchen.log", ios::app); //append模式打开日志文件
+    
+    // 记录时间
+    logfile << getCurrentTime() << " ";
+    
+    // 记录订单内容
+    for (size_t i = 0; i < order.size(); ++i) {
+        if (i > 0) logfile << ",";
+        logfile << order[i];
+    }
+    
+    // 记录成功状态
+    logfile << " " << (success ? "1" : "0") << " [";
+    
+    // 记录食材状态
+    bool end = false;
+    for (const auto& [ing, qty] : updates) {
+        if (end){
+             logfile << "; ";
         }
-        bool close_client(){
-            if(m_clientfd == -1){
-                return false;
-            }
-            ::close(m_clientfd);
-            m_clientfd = -1;
-            return true;
+        logfile << ing << " " << qty;
+        end = true;
+    }
+    logfile << "]" << endl;
+    
+    logfile.close();
+}
+
+bool processOrder(const vector<int>& order, map<string, int>& updates) {
+    lock_guard<mutex> lock(inventory_mutex);
+    bool allSuccess = true;
+
+    // 检查所有食物ID是否有效
+    for (int id : order) {
+        if (foods.find(id) == foods.end()) {
+            return false;
         }
-//解析报文
-        bool parseMessage(string& buffer){
-            MessageHeader msgheader;
-            memcpy(&msgheader,buffer.data(), sizeof(MessageHeader));
-            if(msgheader.msgType != ORDER_REQUEST){
-                cout <<"msgheader"<< endl;
-                return false;
-            }
-            OrderHeader orderheader;
-            memcpy(&orderheader, buffer.data(), sizeof(OrderHeader));
-            int offset = sizeof(MessageHeader) + sizeof(OrderHeader);
-            
-            OrderItem items[orderheader.itemCount];
-            for (int i = 0; i < orderheader.itemCount; i++) {
-                memcpy(&items[i], buffer.data() + offset, sizeof(OrderItem));
-                offset += sizeof(OrderItem);
-            }
-            cout << "foodID" << items->foodId << endl;
-            return true;
+    }
+
+    // 收集总需求
+    map<string, int> totalDemand;
+    for (int id : order) {
+        const FoodItem& item = foods[id];
+        for (const string& ing : item.ingredients) {
+            totalDemand[ing]++;
         }
-        ~ ctcpserver(){
-            close_listen();
-            close_client();
+    }
+
+    // 检查库存是否足够
+    for (const auto& [ing, demand] : totalDemand) {
+        if (inventory.find(ing) == inventory.end() || inventory[ing] < demand) {
+            allSuccess = false;
+            break;
         }
+    }
+
+    // 记录原始库存状态
+    for (const auto& [ing, _] : totalDemand) {
+        updates[ing] = inventory[ing];
+    }
+
+    if (allSuccess) {
+        // 扣减库存
+        for (const auto& [ing, demand] : totalDemand) {
+            inventory[ing] -= demand;
+            updates[ing] = inventory[ing]; // 更新记录为扣减后的库存
+        }
+    }
+
+    return allSuccess;
+}
+//处理客户端请求
+void handleClient(int clientSocket) {
+    char buffer[1024] = {0};
+    int bytesRead = recv(clientSocket, buffer, sizeof(buffer), 0);
+    if (bytesRead <= 0) {
+        close(clientSocket);
+        return;
+    }
+
+    vector<int> order;
+    stringstream ss(buffer);
+    string item;
+    // 解析客户端请求
+    while (getline(ss, item, ',')) {
+       order.push_back(stoi(item));
+    } 
+
+    map<string, int> updates;
+    bool success = processOrder(order, updates);
+    
+    string response = success ? "1" : "-1";
+    send(clientSocket, response.c_str(), response.size(), 0);
+    //生成日志
+    logOrder(order, success, updates);
+    close(clientSocket);
+}
+
+int main() { 
+    loadFoods("foods.txt");
+    loadInventory("inventory.txt");
+    // 创建服务器套接字
+    int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in address{};
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(serverFd, (struct sockaddr*)&address, sizeof(address));
+    listen(serverFd, 5);
+
+    cout << "8080端口启动成功" << endl;
+
 
     
-};
-
-int main(int argc, char *argv[]){
-    if(argc != 2){
-        cout << "端口错误" << endl;
-        return -1;
-    }
-
-    ctcpserver tcpserver;
-    if(tcpserver.init_server(atoi(argv[1])) == false){
-        perror("init_server()");
-        return -1;
-    }
-
-    if(tcpserver.accept() == false){
-        perror("accept()");
-        return -1;
-    }
-    cout << "客户端已连接。\n";
-
-    string buffer;
-    while (true)
-    {
-
+    while (true) {
+        sockaddr_in clientAddr{};
+        socklen_t addrLen = sizeof(clientAddr);
+        int clientSocket = accept(serverFd, (struct sockaddr*)&clientAddr, &addrLen);
         
-        if((tcpserver.recv(buffer, 1024)) == false){
-            perror("recv()");
-            return -1;
+        if (clientSocket < 0) {
+            cerr << "接受失败" << endl;
+            continue;
         }
 
-        if (buffer.size() < sizeof(MessageHeader)) {
-            cerr << "buffer" << endl;
-            return -1;
-        }
-        cout << "已接收" << endl;
-
-
-        if(tcpserver.parseMessage(buffer) == false){
-            cerr << "parseMessage()" << endl;
-            return -1;
-        }
-
-        cout << "已解析" << endl;
-
-    //    MessageHeader msgheader;
-    //    memcpy(&msgheader,buffer.data(), sizeof(MessageHeader));
-    //    if(msgheader.msgType != ORDER_REQUEST){
-    //        cout <<"msgheader"<< endl;
-    //        return -1;
-    //    }
-    //    OrderHeader orderheader;
-    //    memcpy(&orderheader, buffer.data(), sizeof(OrderHeader));
-    //    int offset = sizeof(MessageHeader) + sizeof(OrderHeader);
-       
-    //    OrderItem items[orderheader.itemCount];
-    //    for (int i = 0; i < orderheader.itemCount; i++) {
-    //        memcpy(&items[i], buffer.data() + offset, sizeof(OrderItem));
-    //        offset += sizeof(OrderItem);
-    //    }
-
-
-        buffer = "ok";
-
-            if((tcpserver.send(buffer)) == false){
-                perror("send()");
-                return -1;
-            }
-            cout << "发送：" << buffer << endl;
-        }
-        
+        thread(handleClient, clientSocket).detach(); //分离线程处理客户端请求
     }
 
+    return 0;
+}
