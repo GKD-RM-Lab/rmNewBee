dfa0e6c824ac53ed0bcab5e27c7f7fb313308cb0
nyiuu <3055316048@qq.com>
2025-04-12 12:16:51 +0800

    _

--- code/cyh/week5/client.cpp
+++ code/cyh/week5/client.cpp
@@ -19,13 +19,55 @@ mutex coutMutex;
 // 读取菜单
 void loadMenu(const string& filename) {
     ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "无法打开文件 " << filename << endl;
+        return;
+    }
     for (string line; getline(file, line); ) {
         istringstream iss(line);
         int num;
-        if (iss >> num) {  // 自动读取第一个数字（制表符前的内容）
+        if (iss >> num) {  // 自动读取第一个数字
             menuItems.push_back(num);
         }
     }
+}
+
+int createSocket() {
+    lock_guard<mutex> lock(coutMutex);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        cerr << "创建socket失败: " << strerror(errno) << endl;
+    }
+    return sock;
+}
+
+bool configureSocket(int sock) {
+    lock_guard<mutex> lock(coutMutex);
+    timeval timeout{5, 0};
+    if (setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout)) < 0 ||
+        setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout)) < 0) {
+        cerr << "设置超时失败: " << strerror(errno) << endl;
+        return false;
+    }
+    return true;
+}
+
+bool connect(int sock, int customerId) {
+    lock_guard<mutex> lock(coutMutex);
+    sockaddr_in servAddr{};
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+    servAddr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &servAddr.sin_addr);
+
+    if (connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
+        lock_guard<mutex> lock(coutMutex);
+        cerr << "顾客 " << customerId << ": 连接服务器失败: " << strerror(errno) << endl;
+        close(sock);
+        sock = -1;
+        return false;
+    }
+    return true;
 }
 
 void placeOrder(int customerId) {
@@ -39,38 +81,21 @@ void placeOrder(int customerId) {
     }
 
     // 创建socket
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock < 0) {
-        lock_guard<mutex> lock(coutMutex);
-        cerr << "顾客 " << customerId << ": 创建套接字失败" << endl;
+    int sock = createSocket();
+    if (sock < 0){
         return;
     }
-    //关闭端口  
-    int reuse = 1;
-    if (setsockopt(sock, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
-        shutdown(sock, SHUT_RDWR);
+    // 配置socket
+    if (!configureSocket(sock)) {
         close(sock);
         return;
     }
-
-    // 设置连接超时（5秒）
-    timeval timeout{5, 0};
-    setsockopt(sock, SOL_SOCKET, SO_RCVTIMEO, &timeout, sizeof(timeout));
-    setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
-
-    // 连接服务器
-    sockaddr_in servAddr{};
-    servAddr.sin_family = AF_INET;
-    servAddr.sin_port = htons(8080);
-    inet_pton(AF_INET, "127.0.0.1", &servAddr.sin_addr);
-
-    if (connect(sock, (struct sockaddr*)&servAddr, sizeof(servAddr)) < 0) {
-        lock_guard<mutex> lock(coutMutex);
-        cerr << "顾客 " << customerId << ": 连接服务器失败 - " << strerror(errno) << endl;
-        shutdown(sock, SHUT_RDWR);
+    // 绑定端口并监听
+    if (!connect(sock, customerId)) {
         close(sock);
         return;
     }
+    
 
     // 准备订单数据
     stringstream orderStream;
@@ -79,15 +104,7 @@ void placeOrder(int customerId) {
         orderStream << order[i];
     }
     string orderStr = orderStream.str();
-
-    // 发送订单
-    if (send(sock, orderStr.c_str(), orderStr.size(), 0) < 0) {
-        lock_guard<mutex> lock(coutMutex);
-        cerr << "顾客 " << customerId << ": 发送订单失败" << endl;
-        shutdown(sock, SHUT_RDWR);
-        close(sock);
-        return;
-    }
+    send(sock, orderStr.c_str(), orderStr.size(), 0);
 
     // 接收响应
     char buffer[1024] = {0};
